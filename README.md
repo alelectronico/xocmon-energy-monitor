3d2c2701172e7fec1242b5a9c3c5ca41a412811a
MM <plusmartin@gmail.com>
2020-09-15 17:53:19 -0500

    Start

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gateway ESP 32
+Gateway for collect data from the sensors and send it in the cloud
---
+++ src/configs.h
@@ -0,0 +1,58 @@
+#ifndef configs_h
+#define configs_h
+
+//google publish
+ #include <HTTPClient.h>
+String GOOGLE_SCRIPT_ID = "AKfycbyZuLmLX83geo0c0nQnzlz3w-uQtoSzJZZNBtQ7vGNWw62HEhAx"; // pupu2 Replace by your GAS service id
+//String GOOGLE_SCRIPT_ID = "AKfycbwKJ4eFVA7c7tfNMuVmW7criLQvSzuioWzPl05NjJLABL6l0W79"; // m001 Replace by your GAS service id
+//String GOOGLE_SCRIPT_ID = "AKfycbwvHLFtKlmxn1JLKSxQUXQtdGqMFZi6u8bgFvPCfZ_WAnRFYIVL"; // pupu Replace by your GAS service id
+
+
+// Default Wifi credentials
+//const char *WIFI_SSID = "AGUACATE";
+//const char *WIFI_PASSWORD = "chipotle";
+//const char * WIFI_SSID = "FLYX";
+//const char * WIFI_PASSWORD = "infinito";
+const char *WIFI_SSID = "Mm";
+const char *WIFI_PASSWORD = "12481632";
+//const char * WIFI_SSID = "AGUACATE";
+//const char * WIFI_PASSWORD = "chipotle";
+
+const int M3TRid = 3;
+
+
+// MQTT information4
+//#define DEVICE_NAME "M3TR"
+// #define AWS_IOT_TOPIC "gprod_test"
+// #define AWS_IOT_ENDPOINT "a3ed10vql0i9ty-ats.iot.us-east-1.amazonaws.com"
+// #define AWS_MAX_RECONNECT_TRIES 5
+
+// LoRa params
+#define SCK 5   // GPIO5  SCK
+#define MISO 19 // GPIO19 MISO
+#define MOSI 27 // GPIO27 MOSI
+#define SS 18   // GPIO18 CS
+#define RST 14  // GPIO14 RESET
+
+#define BAND 915E6
+#define SPREADING_FACTOR 12
+#define BAND_WINDTH 500E3
+
+
+
+
+
+// #define AWS_IOT_TOPIC "$aws/things/" DEVICE_NAME
+// const char *WIFI_SSID = "iflyx";
+// const char *WIFI_PASSWORD = "12481632";
+
+// const char *WIFI_SSID = "iflyx";
+// const char *WIFI_PASSWORD = "12481632";
+
+// const char *WIFI_SSID = "FLYX";
+// const char *WIFI_PASSWORD = "infinito";
+
+// const char *WIFI_SSID = "INFINITUM0BEE_2.4_2";
+// const char *WIFI_PASSWORD = "superandy2018";
+
+#endif

cfab40d83db21cb95b52601e86034b2bdde1f0c4
MM <plusmartin@gmail.com>
2020-09-15 19:42:15 -0500

    added google GAS for sheet 03

--- src/configs.h
+++ src/configs.h
@@ -3,10 +3,10 @@
 
 //google publish
  #include <HTTPClient.h>
-String GOOGLE_SCRIPT_ID = "AKfycbyZuLmLX83geo0c0nQnzlz3w-uQtoSzJZZNBtQ7vGNWw62HEhAx"; // pupu2 Replace by your GAS service id
+//String GOOGLE_SCRIPT_ID = "AKfycbyZuLmLX83geo0c0nQnzlz3w-uQtoSzJZZNBtQ7vGNWw62HEhAx"; // pupu2 Replace by your GAS service id
 //String GOOGLE_SCRIPT_ID = "AKfycbwKJ4eFVA7c7tfNMuVmW7criLQvSzuioWzPl05NjJLABL6l0W79"; // m001 Replace by your GAS service id
 //String GOOGLE_SCRIPT_ID = "AKfycbwvHLFtKlmxn1JLKSxQUXQtdGqMFZi6u8bgFvPCfZ_WAnRFYIVL"; // pupu Replace by your GAS service id
-
+String GOOGLE_SCRIPT_ID = "AKfycbydrQCU67gRJiyRCfhZBjQmjzSCFpmc5nGzjDBE"; //03
 
 // Default Wifi credentials
 //const char *WIFI_SSID = "AGUACATE";
